21aea400e69fd17b2d9a38ecd842a3f10237659a
Rachit Bharadwaj <rachitbharadwaj52@gmail.com>
2025-05-18 00:08:52 +0530

    fix: update API routes to use environment variables for production

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,21 +1,21 @@
-// export const HOST = import.meta.env.VITE_SERVER_URL;
+export const PRODUCTION = import.meta.env.PRODUCTION;
 
-export const HOST = "/";
+export const HOST = PRODUCTION ? "/" : import.meta.env.VITE_SERVER_URL;
 
 // routes
-export const AUTH_ROUTES = "api/auth";
+export const AUTH_ROUTES = `${HOST}/api/auth`;
 export const REGISTER_ROUTE = `${AUTH_ROUTES}/register`;
 export const LOGIN_ROUTE = `${AUTH_ROUTES}/login`;
 export const GET_USER_INFO = `${AUTH_ROUTES}/getUserInfo`;
 
-export const CONTACT_ROUTES = "api/contact";
+export const CONTACT_ROUTES = `${HOST}/api/contact`;
 export const SEARCH_ROUTE = `${CONTACT_ROUTES}/search`;
 export const GET_CONTACTS = `${CONTACT_ROUTES}/get-contacts`;
 
-export const CHAT_ROUTES = "api/chat";
+export const CHAT_ROUTES = `${HOST}/api/chat`;
 export const GET_MESSAGES = `${CHAT_ROUTES}/messages`;
 
-export const USER_ROUTES = "api/user";
+export const USER_ROUTES = `${HOST}/api/user`;
 export const FETCH_BY_USERNAME = `${USER_ROUTES}/fetchByUserName`;
 export const EDIT_PROFILE = `${USER_ROUTES}/editProfile`;
 export const CHECK_USERNAME = `${USER_ROUTES}/checkUsername`;
